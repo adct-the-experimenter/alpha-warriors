fb7be45dcbcf02db625d5fbadf9a24756e20e613
Pablo Antonio Camacho Jr <bringerofawesomefood@gmail.com>
2022-02-19 13:49:50 -0700

    quick fix for falling and attack in freeplay mode.

--- src/systems/PhysicsSystem.cpp
+++ src/systems/PhysicsSystem.cpp
@@ -723,7 +723,8 @@ void PhysicsSystem::Update_MetroidVaniaMode(float& dt)
 				}
 				
 				//if entity is flying and performing a no move attack, stop player.
-				if(gen_entity_state.actor_state == EntityState::ATTACKING_NO_MOVE )
+				if(gen_entity_state.actor_state == EntityState::ATTACKING_NO_MOVE && 
+				   rigidBody.in_flying_state)
 				{
 					rigidBody.velocity.x = 0;
 					rigidBody.velocity.y = 0;
